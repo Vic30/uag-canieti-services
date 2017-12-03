30aba8bfff028edcf60c0829ed5f83b33affc7a4
victh30 <lrvictor@mx1.ibm.com>
2017-12-02 23:54:28 -0600

    Function to store a new registry

--- src/main/java/mx/uag/canieti/Canieti/project/RegisterController.java
+++ src/main/java/mx/uag/canieti/Canieti/project/RegisterController.java
@@ -1,6 +1,8 @@
 package mx.uag.canieti.Canieti.project;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,15 +10,21 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import mx.uag.canieti.Canieti.model.RegistryModel;
+
 @RestController
 @RequestMapping("/api/register")
 public class RegisterController {
 	
-	@GetMapping
-	private JsonNode example(){
-		ObjectMapper mapper = new ObjectMapper();
-		ObjectNode jNode = mapper.createObjectNode();
-		jNode.put("Text", "Hello World!");
-		return jNode;
+	@PostMapping
+	private RegistryModel example(@RequestBody RegistryModel registry){
+		validateRegistry(registry);
+		//Store in DB
+		return registry;
+	}
+	
+	private void validateRegistry(RegistryModel registry) {
+		//check if exists in DB
+		//If yes return an exception
 	}
 }
