f7884db50d54f95d1ad3918c8874567354d7ab73
Laura Esther Lopez <esther_509@hotmail.com>
2017-12-05 13:22:46 -0600

    Changed http status code for invalid requests, and added vaidation for empty paramaters

--- src/main/java/mx/uag/canieti/Canieti/Controller/RegisterController.java
+++ src/main/java/mx/uag/canieti/Canieti/Controller/RegisterController.java
@@ -1,6 +1,8 @@
 package mx.uag.canieti.Canieti.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,24 +18,42 @@ public class RegisterController {
 	private CloudantManager clientDB;
 	
 	@PostMapping
-	private Object example(@RequestBody RegistryModel registry){
+	private ResponseEntity<String> example(@RequestBody RegistryModel registry){
 		
 		try {
 			validateRegistry(registry);
 		}catch(Exception e) {
-			return "{ \"error_code:\" : 1, \"error\": \"Already registered\" }";
+			String json = String.format("{\"error\": \"%s\" }", e.getMessage());
+			return ResponseEntity.status(HttpStatus.CONFLICT).body(json);
 		}
 		
 		try {
 			
 			clientDB.save(registry);
 		} catch (Exception e) {
-			return "{ \"error_code:\" : 2, \"error\": \"Error saving the data.\" }";
+			String json = "{ \"error\": \"Internal error when try to save the data.\" }";
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(json);
 		}
-		return registry;
+		
+		String json = String.format("{ \"name\": \"%s\", \"email\": \"%s\", \"phone\": \"%s\"}", registry.getName(), registry.getEmail(), registry.getPhone());
+		
+		return ResponseEntity.ok(json);
 	}
 	
 	private void validateRegistry(RegistryModel registry) throws Exception {
+		
+		if(registry.getName().isEmpty()) {
+			throw new Exception("Name is empty");
+		}
+		
+		if(registry.getEmail().isEmpty()) {
+			throw new Exception("Email is empty");
+		}
+		
+		if(registry.getPhone().isEmpty()) {
+			throw new Exception("Phone is empty");
+		}
+		
 		if(clientDB.exists(registry)) {
 			throw new Exception("This element already exist.");
 		}
